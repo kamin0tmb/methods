0ef59a96b3067b22832c251dbd735ecf6e12474d
kamin0 <jm187@yandex.ru>
2022-04-14 18:54:32 +0300

    Добавьте файлы проекта.

---
+++ methods/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var favcolors = new string [3];
+            for (int i = 0; i < 3; i++)
+            favcolors[i] = ShowColor(); 
+        }
+        static string ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                string color = Console.ReadLine();
+            
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
+            return color;
+        }
+}
+}

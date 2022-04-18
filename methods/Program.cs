923aa0c6d3139039037be840a043fd22a4624e04
kamin0 <jm187@yandex.ru>
2022-04-18 19:32:13 +0300

    5.3.11

--- methods/Program.cs
+++ methods/Program.cs
@@ -14,7 +14,14 @@ namespace methods
             GetAge(out SomeName, out SomeAge);
             Console.WriteLine(SomeName);
             Console.WriteLine(SomeAge);
-
+            Console.WriteLine("Введите число 1: ");
+            int num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число 2: ");
+            int num2 = Int32.Parse(Console.ReadLine());
+            int num3;
+            int num4 = 2;
+            int summ = SumNumbers(ref num1, in num2, out num3, num4);
+            Console.WriteLine(summ);
             static void GetName(ref string name)
             {
                 Console.WriteLine("Введите имя: ");
@@ -26,6 +33,14 @@ namespace methods
                 name = Console.ReadLine();
                 Console.WriteLine("Введите возрвст: ");
                 age = byte.Parse(Console.ReadLine());
+            }
+
+            static int SumNumbers(ref int num1, in int num2, out int num3, int num4)
+            {
+                
+                num3 = num1 + num2;
+                var result = num3 * num4;
+                return result;
             }
         }
     }

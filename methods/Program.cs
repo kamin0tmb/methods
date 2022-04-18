50e850fc5307c97fab662c95048a4f8604566011
kamin0 <jm187@yandex.ru>
2022-04-18 21:48:33 +0300

    5.5.3

--- methods/Program.cs
+++ methods/Program.cs
@@ -1,89 +1,32 @@
 ﻿using System;
 
-namespace methods
+class MainClass
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Напишите что-то");
+        var str = Console.ReadLine();
+
+        Console.WriteLine("Укажите глубину эха");
+        var deep = int.Parse(Console.ReadLine());
+
+        Echo(str, deep);
+
+        
+    }
+
+    static void Echo(string saidword, int deep)
+    {
+        string modif = saidword;
+        Console.WriteLine("..." + modif);
+        if (modif.Length > 2)
         {
-
-            int[] array = GetArrayFromConsole();
-            int[] sorteddesc;
-            int[] sortedasc;
-            SortArray(in array, out sorteddesc, out sortedasc);
-            Console.WriteLine("Исходный массив: ");
-            ShowArray(array);
-            Console.WriteLine("Отсортированный массив от меньшего к большему: ");
-            ShowArray(sortedasc);
-            Console.WriteLine("Отсортированный массив от большего к меньшему: ");
-            ShowArray(sorteddesc);
-
-
+            modif = modif.Remove(0, 2);
         }
-        static int[] GetArrayFromConsole()
+        if (deep > 1)
         {
-            var result = new int[10];
-
-            for (int i = 0; i < result.Length; i++)
-            {
-                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-                result[i] = int.Parse(Console.ReadLine());
-            }
-            Console.WriteLine();
-            return result;
-
-        }
-        static int[] SortArrayAsc(int[] array)
-        {
-            int[] result = new int[array.Length];
-            Array.Copy(array, result, array.Length);
-            int temp = 0;
-            for (int i = 0; i < result.Length; i++)
-            {
-                for (int j = i + 1; j < result.Length; j++)
-                    if (result[i] > result[j])
-                    {
-                        temp = result[i];
-                        result[i] = result[j];
-                        result[j] = temp;
-                    }
-
-            }
-
-            return result;
-        }
-        static int[] SortArrayDesc(int[] array)
-        {
-            int[] result = new int[array.Length];
-            Array.Copy(array, result, array.Length);
-            int temp = 0;
-            for (int i = 0; i < result.Length; i++)
-            {
-                for (int j = i + 1; j < result.Length; j++)
-                    if (result[i] < result[j])
-                    {
-                        temp = result[i];
-                        result[i] = result[j];
-                        result[j] = temp;
-                    }
-
-            }
-
-            return result;
-        }
-        static void SortArray (in int[] array, out int[] sorteddesc, out int[] sortedasc)
-        {
-            sortedasc = SortArrayAsc(array);
-            sorteddesc = SortArrayDesc(array);
+            Echo(modif, deep - 1);
             
-        }
-        static void ShowArray(int[] array)
-        {
-            for (int i = 0; i < array.Length; i++)
-                Console.WriteLine(array[i]);
-            Console.WriteLine();
-
         }
     }
 }
-
